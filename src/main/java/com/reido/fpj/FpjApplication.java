0857f81d769084b4fe70dcb9c02d869821a36863
ReidoBoss <bryansagarino222@gmail.com>
2025-05-08 08:37:27 +0800

    Spring Boot Initial Commit

---
+++ src/main/java/com/reido/fpj/FpjApplication.java
@@ -0,0 +1,13 @@
+package com.reido.fpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpjApplication.class, args);
+	}
+
+}
